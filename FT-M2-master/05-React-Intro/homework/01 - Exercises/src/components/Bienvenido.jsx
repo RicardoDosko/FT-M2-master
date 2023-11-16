78832b84dce9f57103c15d112c4149554a49d7d7
RNDOSKO <Ricardo.dosko@gmail.com>
2023-11-16 01:39:49 -0300

    Homework 03: fix and Homework 05:React-Intro-01 exercises

--- FT-M2-master/05-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ FT-M2-master/05-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import Btn from "./Botones";
 
-const studentName = "";
+
+const studentName = "Ricardo Nery Dosko";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  
+    return(
+      <div>
+        <h1>Puerco Araña</h1>
+        <h3>Mi nombre es: {studentName}</h3>
+        {techSkills.map((skill, index) => (
+          <li key={index}>{skill}</li> 
+        ))}
+        <Btn alerts={alerts} />
+        
+      </div>
+    );
+    
 }
 
 // Esto lo exportamos para los tests
---
+++ FT-M2-master/05-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Botones extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => alert(this.props.alerts.m1)}>Módulo 1</button>
+        <button onClick={() => alert(this.props.alerts.m2)}>Módulo 2</button>
+      </div>
+    );
+  }
+}
+
+export default Botones;
